1688ce8a2fa4fbc1e592255d4cd3671aa59f58e4
sngo318 <47369260+sngo318@users.noreply.github.com>
2019-10-10 11:29:00 -0400

    Create stephanie_hap.py

---
+++ stephanie_hap.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=1-00:00
+#SBATCH --mem=8000M
+#SBATCH --job-name="hap.py 60x test"
+
+source $HOME/hap.py/bin/activate
+
+hap.py HG001_GRCh37_NA12878.vcf scratch/vcf/HuRef60x.testmem.vcf -r human_g1k_v37.fasta -o test.hap.py.60x
