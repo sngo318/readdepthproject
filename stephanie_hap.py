b26a1ab4f71559532147de7c8728b5d086bae26f
sngo318 <47369260+sngo318@users.noreply.github.com>
2019-10-10 11:41:17 -0400

    Update stephanie_hap.py

--- stephanie_hap.py
+++ stephanie_hap.py
@@ -5,6 +5,12 @@
 #SBATCH --mem=8000M
 #SBATCH --job-name="hap.py 60x test"
 
+export HGREF=~./projects/def-msteiner/readdepth/ref_genome/human_g1k_v37.fa
+echo $HGREF
+
 source $HOME/hap.py/bin/activate
 
 hap.py HG001_GRCh37_NA12878.vcf scratch/vcf/HuRef60x.testmem.vcf -r human_g1k_v37.fasta -o test.hap.py.60x
+
+echo "output"
+ls test.hap.py.60*
